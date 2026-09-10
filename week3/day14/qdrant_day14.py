c641d7c8527daa85be2eb1494f7bd0221caa1e2a
karakRohan <karakrohan2005@gmail.com>
2026-09-10 19:29:20 +0530

    add

--- week3/day14/qdrant_day14.py
+++ week3/day14/qdrant_day14.py
@@ -188,7 +188,8 @@ If the answer is not present in the context, say:
 """
 
     response = groq_client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        # model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-20b",
         messages=[
             {
                 "role": "user",
